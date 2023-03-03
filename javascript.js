ed8059468f9aec17d807eb95b83afb781546bb33
qudys <shuaibqudus33@gmail.com>
2023-03-03 17:48:38 +0100

    Complete playRound function

--- javascript.js
+++ javascript.js
@@ -5,6 +5,27 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice())
 function playGame(playerSelection, computerSelection) {
-    const playerToLower = playerSelection.lowerCase();
-    const computerToLower = computerSelection.lowerCase();
+    const playerToLower = playerSelection.toLowerCase();
+    const computerToLower = computerSelection.toLowerCase();
+    if (playerToLower === 'rock' && computerToLower === 'paper') {
+        return 'You Lose! Paper beats Rock';
+    }
+    else if (playerToLower === 'rock' && computerToLower === 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    }
+    else if (playerToLower === 'paper' && computerToLower === 'rock') {
+        return 'You Win! Paper beats Rock';
+    }
+    else if (playerToLower === 'paper' && computerToLower === 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+    }
+    else if (playerToLower === 'scissors' && computerToLower === 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    }
+    else if (playerToLower === 'scissors' && computerToLower === 'paper') {
+        return 'You Win! Scissors beats Paper';
+    }
+    else {
+        return 'You tied!';
+    }
 }
