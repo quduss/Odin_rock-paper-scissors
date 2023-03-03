ea4383a4f42aa798511a56c77535d2b4da40d870
qudys <shuaibqudus33@gmail.com>
2023-03-03 21:40:08 +0100

    Complete game() function

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ function getComputerChoice() {
     let i = Math.floor(Math.random() * arr.length);
     return arr[i];
 }
-console.log(getComputerChoice())
-function playGame(playerSelection, computerSelection) {
+//console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
     const playerToLower = playerSelection.toLowerCase();
     const computerToLower = computerSelection.toLowerCase();
     if (playerToLower === 'rock' && computerToLower === 'paper') {
@@ -28,4 +28,28 @@ function playGame(playerSelection, computerSelection) {
     else {
         return 'You tied!';
     }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Your selection');
+        let computerSelection = getComputerChoice();
+        let a = playRound(playerSelection, computerSelection);
+        console.log(a);
+        if (
+            a === 'You Lose! Paper beats Rock'
+            || a === 'You Lose! Scissors beats Paper'
+            || a === 'You Lose! Rock beats Scissors'
+        ) {computerScore++;}
+        else if (
+            a === 'You Win! Rock beats Scissors'
+            || a === 'You Win! Paper beats Rock'
+            || a === 'You Win! Scissors beats Paper'
+        ) {playerScore++;}
+    }
+    if (playerScore > computerScore) {console.log("You're the Winner!")}
+    else if (computerScore > playerScore) {console.log("You're the Loser!")}
+    else {console.log("You Tied!")}
 }
