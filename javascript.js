8082b994108a1aca3df97b31fcc971e048834fe6
qudys <shuaibqudus33@gmail.com>
2023-06-23 23:34:14 +0100

    Write algorithm for the scores

--- javascript.js
+++ javascript.js
@@ -4,56 +4,44 @@ function getComputerChoice() {
     return arr[i];
 }
 //console.log(getComputerChoice())
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
     const playerToLower = playerSelection.toLowerCase();
 
     if (playerToLower === 'rock' && computerSelection === 'paper') {
         div.textContent = 'You Lose! Paper beats Rock';
+        computerScore++;
+        
     }
     else if (playerToLower === 'rock' && computerSelection === 'scissors') {
         div.textContent = 'You Win! Rock beats Scissors';
+        playerScore++;
     }
     else if (playerToLower === 'paper' && computerSelection === 'rock') {
         div.textContent = 'You Win! Paper beats Rock';
+        playerScore++;
     }
     else if (playerToLower === 'paper' && computerSelection === 'scissors') {
         div.textContent = 'You Lose! Scissors beats Paper';
+        computerScore++;
     }
     else if (playerToLower === 'scissors' && computerSelection === 'rock') {
         div.textContent = 'You Lose! Rock beats Scissors';
+        computerScore++;
     }
     else if (playerToLower === 'scissors' && computerSelection === 'paper') {
         div.textContent = 'You Win! Scissors beats Paper';
+        playerScore++;
     }
     else {
         div.textContent = 'You tied!';
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Your selection');
-        let computerSelection = getComputerChoice();
-        let a = playRound(playerSelection, computerSelection);
-        console.log(a);
-        if (
-            a === 'You Lose! Paper beats Rock'
-            || a === 'You Lose! Scissors beats Paper'
-            || a === 'You Lose! Rock beats Scissors'
-        ) computerScore++;
-        else if (
-            a === 'You Win! Rock beats Scissors'
-            || a === 'You Win! Paper beats Rock'
-            || a === 'You Win! Scissors beats Paper'
-        ) playerScore++;
-    }
-    if (playerScore > computerScore) console.log("You're the Winner!")
-    else if (computerScore > playerScore) console.log("You're the Loser!")
-    else console.log("You both drew the game!")
-}
-let computerSelection = getComputerChoice();
+
+let playerScore = 0;
+let computerScore = 0;
 const buttons = document.querySelectorAll('button');
 const div = document.querySelector('div');
-buttons.forEach(button => button.addEventListener('click', () => playRound(button.textContent, computerSelection)));
+   
+buttons.forEach(button => button.addEventListener('click', () => playRound(button.textContent)));
