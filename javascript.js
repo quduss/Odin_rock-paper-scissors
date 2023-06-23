7f18e63a20862c441335a63cb6010efe20d968af
qudys <shuaibqudus33@gmail.com>
2023-06-24 00:53:01 +0100

    Complete the game script

--- javascript.js
+++ javascript.js
@@ -7,40 +7,52 @@ function getComputerChoice() {
 function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
     const playerToLower = playerSelection.toLowerCase();
+    winner.textContent = '';
+
 
     if (playerToLower === 'rock' && computerSelection === 'paper') {
         result.textContent = 'You Lose! Paper beats Rock';
         computerScore++;
-        score.textContent = `${playerScore} - ${computerScore}`;
+        score.textContent = `You ${playerScore} - Computer ${computerScore}`;
     }
-    else if (playerToLower === 'rock' && computerSelection === 'scissors') {
+     if (playerToLower === 'rock' && computerSelection === 'scissors') {
         result.textContent = 'You Win! Rock beats Scissors';
         playerScore++;
-        score.textContent = `${playerScore} - ${computerScore}`;
+        score.textContent = `You ${playerScore} - Computer ${computerScore}`;
     }
-    else if (playerToLower === 'paper' && computerSelection === 'rock') {
+     if (playerToLower === 'paper' && computerSelection === 'rock') {
         result.textContent = 'You Win! Paper beats Rock';
         playerScore++;
-        score.textContent = `${playerScore} - ${computerScore}`;
+        score.textContent = `You ${playerScore} - Computer ${computerScore}`;
     }
-    else if (playerToLower === 'paper' && computerSelection === 'scissors') {
+     if (playerToLower === 'paper' && computerSelection === 'scissors') {
         result.textContent = 'You Lose! Scissors beats Paper';
         computerScore++;
-        score.textContent = `${playerScore} - ${computerScore}`;
+        score.textContent = `You ${playerScore} - Computer ${computerScore}`;
     }
-    else if (playerToLower === 'scissors' && computerSelection === 'rock') {
+     if (playerToLower === 'scissors' && computerSelection === 'rock') {
         result.textContent = 'You Lose! Rock beats Scissors';
         computerScore++;
-        score.textContent = `${playerScore} - ${computerScore}`;
+        score.textContent = `You ${playerScore} - Computer ${computerScore}`;
     }
-    else if (playerToLower === 'scissors' && computerSelection === 'paper') {
+     if (playerToLower === 'scissors' && computerSelection === 'paper') {
         result.textContent = 'You Win! Scissors beats Paper';
         playerScore++;
-        score.textContent = `${playerScore} - ${computerScore}`;
+        score.textContent = `You ${playerScore} - Computer ${computerScore}`;
     }
-    else {
+    if (playerToLower === computerSelection) {
         result.textContent = 'You tied!';
-        score.textContent = `${playerScore} - ${computerScore}`;
+        score.textContent = `You ${playerScore} - Computer ${computerScore}`;
+    }
+    if (playerScore === 5) {
+        winner.textContent = 'You have 5 points! You won the game!'
+        playerScore = 0;
+        computerScore = 0;
+    }
+    if (computerScore === 5) {
+        winner.textContent = 'You lost! The Computer won but you can try again.'
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
@@ -50,6 +62,7 @@ let computerScore = 0;
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result');
 const score = document.querySelector('.score');
+const winner = document.querySelector('.winner');
 
    
 buttons.forEach(button => button.addEventListener('click', () => playRound(button.textContent)));
